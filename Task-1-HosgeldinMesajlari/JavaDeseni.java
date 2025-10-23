f29719fd7c2f90eb81bcd75a93d6735ef114b3ea
efecan10 <250541093@firat.edu.tr>
2025-10-23 12:08:59 +0300

    Update JavaDeseni.java

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,22 @@
+package javaapplication5;
+
+public class JavaDeseni {
+    public static void main(String[] args) {
+        System.out.println("         J       a   v     v  a");
+        System.out.println("         J      a a   v   v  a a");
+        System.out.println("     J   J     aaaaa   V V  aaaaa");
+        System.out.println("      J J     a     a   V  a     a");
+        System.out.println();
+        System.out.println("          (  )   (   )  )");
+        System.out.println("           ) (   )  (  (");
+        System.out.println("           ( )  (    ) )");
+        System.out.println("           _____________");
+        System.out.println("          <_____________> ___");
+        System.out.println("          |             |/ _ \\");
+        System.out.println("          |               | | |");
+        System.out.println("          |               |_| |");
+        System.out.println("       ___|             |\\___/");
+        System.out.println("      /    \\___________/    \\");
+        System.out.println("      \\_____________________/");
+    }
+}
