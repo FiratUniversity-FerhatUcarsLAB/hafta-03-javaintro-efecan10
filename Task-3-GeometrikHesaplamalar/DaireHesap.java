e5f1bce17d5ef2880fdfe36be24acf3b9dcf7e7d
efecan10 <250541093@firat.edu.tr>
2025-10-23 18:54:45 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,17 @@
+public class DaireHesap {
+2 public static void main ( String [] args ) {
+3 double r = 5.5;
+4 double pi = 3.14159;
+5
+6 // Alan hesapla
+7 double alan = pi * r * r ;
+8
+9 // Cevre hesapla
+0 double cevre = 2 * pi * r ;
+
+2 // Sonuclari yazdir
+3 System . out . println (" Yaricap : " + r ) ;
+4 System . out . println (" Alan : " + alan ) ;
+5 System . out . println (" Cevre : " + cevre ) ;
+6 }
+7 }
