1808e9ed4ee72ac356bbc9737c6dfafbf5c4be58
efecan10 <250541093@firat.edu.tr>
2025-10-23 12:13:46 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,7 @@
+public class MesajOrnekleri {
+    public static void main(String[] args) {
+        System.out.println("Merhaba, nasılsın?");
+        System.out.println("Bugün hava çok güzel!");
+        System.out.println("Teşekkürler, görüşürüz!");
+    }
+}
