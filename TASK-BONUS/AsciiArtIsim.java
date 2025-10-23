bb5ecdd946c4b0d128f64c7a3b34cef732f65d5e
efecan10 <250541093@firat.edu.tr>
2025-10-23 19:11:37 +0300

    Add AsciiArtIsim.java for ASCII art display

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,9 @@
+public class EfecanAscii {
+    public static void main(String[] args) {
+        System.out.println("EEEE  FFFFF  EEEEE  CCCC   AAAAA  N   N");
+        System.out.println("E     F      E     C      A   A  NN  N");
+        System.out.println("EEE   FFF    EEE   C      AAAAA  N N N");
+        System.out.println("E     F      E     C      A   A  N  NN");
+        System.out.println("EEEE  F      EEEEE  CCCC  A   A  N   N");
+    }
+}
