cf33deda491cf3ea2e78ca13aa6690193cfca700
efecan10 <250541093@firat.edu.tr>
2025-10-23 19:02:24 +0300

    Add IsimKarti class to display student information

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,15 @@
+public class isimKarti {
+    public static void main(String[] args) {
+        String ad = "Efecan";
+        String soyad = "Önal";
+        String ogrenciNo = "250541093";
+        String bolum = "Yazılım Mühendisliği";
+
+        System.out.println("+-------------------------------------+");
+        System.out.printf("| %-12s : %-23s |\n", "Ad", ad);
+        System.out.printf("| %-12s : %-23s |\n", "Soyad", soyad);
+        System.out.printf("| %-12s : %-23s |\n", "Öğrenci No", ogrenciNo);
+        System.out.printf("| %-12s : %-23s |\n", "Bölüm", bolum);
+        System.out.println("+-------------------------------------+");
+    }
+}
