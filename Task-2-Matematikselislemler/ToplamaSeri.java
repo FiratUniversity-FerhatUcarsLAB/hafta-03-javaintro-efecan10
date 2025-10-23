73aae018237eb2f014d12f437243ce493cb4f4f2
efecan10 <250541093@firat.edu.tr>
2025-10-23 18:53:55 +0300

    Add ToplamaSeri class to calculate sum of numbers

---
+++ Task-2-Matematikselislemler/ToplamaSeri.java
@@ -0,0 +1,9 @@
+public class ToplamaSeri {
+2 public static void main ( String [] args ) {
+3 // Sonucu hesapla
+4 int sonuc = 1+2+3+4+5+6+7+8+9;
+5
+6 // Ekrana yazdir
+7 System . out . println (" 1+2+...+9 = " + sonuc ) ;
+8 }
+9 }
