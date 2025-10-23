ff206a0da71c0995c3804e0edbfb80bbca6087f7
efecan10 <250541093@firat.edu.tr>
2025-10-23 19:07:34 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,36 @@
+public class UsluSayilarTablosuTekTek {
+    public static void main(String[] args) {
+        System.out.println("a    a^1   a^2   a^3");
+        System.out.println("-----------------------");
+
+        int a1 = 1;
+        int a1_1 = a1;
+        int a1_2 = a1 * a1;
+        int a1_3 = a1 * a1 * a1;
+        System.out.println(a1 + "    " + a1_1 + "    " + a1_2 + "    " + a1_3);
+
+        int a2 = 2;
+        int a2_1 = a2;
+        int a2_2 = a2 * a2;
+        int a2_3 = a2 * a2 * a2;
+        System.out.println(a2 + "    " + a2_1 + "    " + a2_2 + "    " + a2_3);
+
+        int a3 = 3;
+        int a3_1 = a3;
+        int a3_2 = a3 * a3;
+        int a3_3 = a3 * a3 * a3;
+        System.out.println(a3 + "    " + a3_1 + "    " + a3_2 + "    " + a3_3);
+
+        int a4 = 4;
+        int a4_1 = a4;
+        int a4_2 = a4 * a4;
+        int a4_3 = a4 * a4 * a4;
+        System.out.println(a4 + "    " + a4_1 + "    " + a4_2 + "    " + a4_3);
+
+        int a5 = 5;
+        int a5_1 = a5;
+        int a5_2 = a5 * a5;
+        int a5_3 = a5 * a5 * a5;
+        System.out.println(a5 + "    " + a5_1 + "    " + a5_2 + "    " + a5_3);
+    }
+}
