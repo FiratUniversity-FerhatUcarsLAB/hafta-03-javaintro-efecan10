9c243b5cd6bcff1ffa4ada3f49abc38f798d1670
efecan10 <250541093@firat.edu.tr>
2025-10-23 19:05:22 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,25 @@
+public class MilToKilometreUzun {
+    public static void main(String[] args) {
+        double ceviriOrani = 1.609;
+
+        double mil1 = 1;
+        double km1 = mil1 * ceviriOrani;
+        System.out.println(mil1 + " mil = " + km1 + " kilometre");
+
+        double mil5 = 5;
+        double km5 = mil5 * ceviriOrani;
+        System.out.println(mil5 + " mil = " + km5 + " kilometre");
+
+        double mil10 = 10;
+        double km10 = mil10 * ceviriOrani;
+        System.out.println(mil10 + " mil = " + km10 + " kilometre");
+
+        double mil20 = 20;
+        double km20 = mil20 * ceviriOrani;
+        System.out.println(mil20 + " mil = " + km20 + " kilometre");
+
+        double mil50 = 50;
+        double km50 = mil50 * ceviriOrani;
+        System.out.println(mil50 + " mil = " + km50 + " kilometre");
+    }
+}
